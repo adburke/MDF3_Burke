12e5c1410c12e6dc6e374a2e9c587639d3fd58d9
adburke <adburke@fullsail.edu>
2014-01-15 22:56:27 -0600

    Wired up picture taking intent with button

--- Week-2/LifeMap/LifeMap/src/main/java/com/example/lifemap/MainDisplayActivity.java
+++ Week-2/LifeMap/LifeMap/src/main/java/com/example/lifemap/MainDisplayActivity.java
@@ -1,30 +1,52 @@
 package com.example.lifemap;
 
 import android.content.Context;
+import android.content.Intent;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 
-public class MainDisplayActivity extends ActionBarActivity implements LocationListener {
+public class MainDisplayActivity extends ActionBarActivity implements LocationListener, View.OnClickListener {
 
+    // Google map
     GoogleMap gMap;
+
+    // Location variables
     Location currentLocation;
     LocationManager locManager;
     Context mContext;
+
+    // UI variables
+    Button captureBtn;
+
+    // Image capture variables
+    private static final int CAPTURE_IMAGE_REQ_CODE = 100;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+    private Uri mediaUri;
 
 
     @Override
@@ -33,6 +55,8 @@ public class MainDisplayActivity extends ActionBarActivity implements LocationLi
         setContentView(R.layout.activity_main);
 
         mContext = this;
+
+        captureBtn = (Button) findViewById(R.id.captureBtn);
 
         gMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         gMap.setMyLocationEnabled(true);
@@ -51,7 +75,7 @@ public class MainDisplayActivity extends ActionBarActivity implements LocationLi
             String provider = locManager.getBestProvider(criteria, true);
             // Poll at a rate of 15s and 0m moved in distance
             // Would have to think about this per application for battery and how often
-            //    location updates would be required
+            // location updates would be required
             locManager.requestLocationUpdates(provider, 15*1000, 0, this);
         }
 
@@ -87,7 +111,12 @@ public class MainDisplayActivity extends ActionBarActivity implements LocationLi
     public void onLocationChanged(Location location) {
         Log.i("MainDisplayActivity", "Lat: " + location.getLatitude() + " -- Long: " + location.getLongitude());
         // Update my current loc variable when onLocationChanged is called.
-        currentLocation = location;
+        // Dirty check to see if location is valid would need a more thorough check
+        // because location could become stale
+        if (location != null) {
+            currentLocation = location;
+        }
+
     }
 
     @Override
@@ -103,5 +132,64 @@ public class MainDisplayActivity extends ActionBarActivity implements LocationLi
     @Override
     public void onProviderDisabled(String provider) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.equals(captureBtn)) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            mediaUri = getOutputFileUri(MEDIA_TYPE_IMAGE);
+            if (mediaUri != null) {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, mediaUri);
+            }
+        }
+    }
+    // Return a file URI from the created output file name and location
+    private static Uri getOutputFileUri(int type) {
+        File mediaFile = createOutputFile(type);
+        if (mediaFile != null) {
+            return Uri.fromFile(mediaFile);
+        } else {
+            return null;
+        }
+    }
+
+    // Create a File for saving based on file type passed in
+    private static File createOutputFile(int type) {
+
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "LifeMap");
+
+        // Create the storage directory if it does not exist
+        if (! storageDir.exists()) {
+            if (! storageDir.mkdirs()) {
+                Log.i("LifeMap", "Failed in creating directory");
+                // End method and return null
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(storageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(storageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_REQ_CODE) {
+            if (resultCode == RESULT_OK) {
+                Log.i("MainDisplayActivity", "Picture saved!");
+            }
+        }
     }
 }
