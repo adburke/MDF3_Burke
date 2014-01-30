4a1f69ab091c7fef9fddabee4c7e79abeb88f30f
adburke <adburke@fullsail.edu>
2014-01-29 22:53:37 -0600

    Work on javascript interface

--- Week-4/WebContact/WebContact/src/main/java/com/adburke/webcontact/MainActivity.java
+++ Week-4/WebContact/WebContact/src/main/java/com/adburke/webcontact/MainActivity.java
@@ -2,21 +2,46 @@ package com.adburke.webcontact;
 
 import android.app.Activity;
 import android.app.ActionBar;
-import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.provider.Contacts;
+import android.provider.ContactsContract;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
+
+    WebView contactWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        WebSettings webSettings = contactWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+    }
+
+    public class ContactAppInterface {
+        Context mContext;
+
+        ContactAppInterface(Context context) {
+            mContext = context;
+        }
+
+        @JavascriptInterface
+        public void saveContact(String name, String phone, String email, String address) {
+            Intent intent = new Intent(Contacts.Intents.Insert.ACTION);
+            intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+        }
 
     }
 
