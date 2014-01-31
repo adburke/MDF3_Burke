8a74add83127d8a7e5535256c5ea8af2e17160cd
adburke <adburke@fullsail.edu>
2014-01-30 19:25:28 -0600

    Added form validation

--- Week-4/WebContact/WebContact/src/main/assets/js/form.js
+++ Week-4/WebContact/WebContact/src/main/assets/js/form.js
@@ -1,11 +1,36 @@
-$( "#addContactBtn").on("click", function() {
-    console.log( "Add Contact Clicked" );
-    var name = $('#nameInput').val();
-    var number = $('#phoneInput').val();
-    var email = $('#emailInput').val();
-    var address = $('#addressInput').val();
-    console.log('Name = ' + name + ' number = ' + number);
-    console.log('Email = ' + email + ' Address = ' + address);
+$(document).ready(function () {
 
-    Android.saveContact(name,number,email,address);
+    $( "#addContactBtn").on("click", function() {
+        console.log( "Add Contact Clicked" );
+        var name = $('#nameInput').val();
+        var number = $('#phoneInput').val();
+        var email = $('#emailInput').val();
+        var address = $('#addressInput').val();
+        console.log('Name = ' + name + ' number = ' + number);
+        console.log('Email = ' + email + ' Address = ' + address);
+
+        Android.saveContact(name,number,email,address);
+    });
+
+    // Form Validation
+    $( "#contactForm").validate({
+        rules: {
+            nameInput: {
+                required: true,
+                lettersonly: true
+            },
+            phoneInput: {
+                required: true,
+                phoneUS: true
+            },
+            emailInput: {
+                required: true,
+                email: true
+            },
+            addressInput: {
+                required: true,
+            }
+        }
+    });
+
 });
